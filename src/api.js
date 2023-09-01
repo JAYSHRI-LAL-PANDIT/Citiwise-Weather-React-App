0221fad601f8c15dc7f62cbe0ddbb656f6b31f9a
Jay <jayshrilalbca@gmail.com>
2023-09-01 12:37:48 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ae0ad6019emsh05a97058c551ffdp1d8917jsn02a1c18fd489",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "079797b1af8607df9f508f2d06d907d3";
